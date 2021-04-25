944d7a8b8b6c7b74f2ee51aec15eb44085a748d6
gekomad <giuseppe.cannella@gmail.com>
2021-04-25 20:32:00 +0200

    .

--- util/git_pull.sh
+++ util/git_pull.sh
@@ -1,2 +1,3 @@
-../remote/bash.sh "git stash;rm -fr .git;mv ~/.bashrc ~/.bashrc_old;rm -fr /tmp/cinnamon_test;cd /tmp;git clone https://github.com/gekomad/cinnamon_test.git ;mv /tmp/cinnamon_test/.git ~;cd ~; git checkout .; git pull --prune"
+bash.sh "cd ~;git stash;rm -fr ~/.git;mv ~/.bashrc ~/.bashrc_old;rm -fr /tmp/cinnamon_test;cd /tmp;git clone https://github.com/gekomad/cinnamon_test.git ;mv /tmp/cinnamon_test/.git ~;cd ~; git checkout .; git pull --prune"
+
 
