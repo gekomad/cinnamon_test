a83e0ac569d141e1220218ff1b831dd5c3911210
gekomad <giuseppe.cannella@gmail.com>
2021-04-26 09:41:58 +0200

    .

--- remote/git_pull.sh
+++ remote/git_pull.sh
@@ -1,2 +1,2 @@
-bash_async.sh "rm -fr /tmp/cinnamon_test;cd /tmp;git clone https://github.com/gekomad/cinnamon_test.git ;mv /tmp/cinnamon_test/.git ~;cd ~; git checkout .; git pull --prune"
+bash_async.sh "cd ~; git stash; git pull --prune;git stash clear"
 
