3dcb44ff3fe9cb3a24b5583927b6a41d5198d065
gekomad <giuseppe.cannella@gmail.com>
2021-06-20 19:14:40 +0200

    1

--- Kill.sh
+++ Kill.sh
@@ -3,7 +3,7 @@ touch ~/STOP_CUTEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
 #kill -9 $(ps aux | grep '[r]andom.sh' | awk '{print $2}')
 #kill -9 $(ps aux | grep '[g]o.sh' | awk '{print $2}')
 killall cutechess-cli 2>/dev/null
-killall -s 9 cutechess-cli cc1plus go.sh stockfish cheng crafty cinnamon cute.sh inotifywait cutechess-cli 2>/dev/null
+killall -s 9 bash_async.sh tar cutechess-cli cc1plus go.sh stockfish cheng crafty cinnamon cute.sh inotifywait cutechess-cli 2>/dev/null
 rm /tmp/.cutelock
 #rm /tmp/gauntlet_sh.lockfile
 #kill -TERM -$(pgrep -o my-script.sh)
