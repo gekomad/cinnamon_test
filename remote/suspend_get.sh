ab7c155889cff531d27de5c43d9c9630c49a70c6
gekomad <giuseppe.cannella@gmail.com>
2021-08-09 16:18:35 +0200

    .

--- remote/suspend_get.sh
+++ remote/suspend_get.sh
@@ -1,4 +1,4 @@
 
-ssh -p 2222 geko@2.235.247.134 "find /home/geko/cinnamon_test/test/ -name suspend|sort -u"
+ssh -p 2222 geko@2.235.247.134 "find /home/geko/cinnamon_test/test/ -name suspend|sort|cut -f6 -d"/""
 exit 0
 
--- remote/suspend_remove_all.sh
+++ remote/suspend_remove_all.sh
@@ -1,8 +1,3 @@
-if [ "" ==	 "$1" ] ; then
-    echo "parameter not found"
-	exit 1
-fi
-
 bash_async.sh "find /home/geko/cinnamon_test/test/ -name suspend -delete"
 exit 0
 
